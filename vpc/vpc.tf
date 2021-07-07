59a7cfddf5dc12c632b6563ad975b2752a57b821
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-07 23:05:17 +0530

    made changes in Makefile

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,9 +1,18 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.VPC_CIDR
-  instance_tenancy = "default"
+  cidr_block                         = var.VPC_CIDR
+  instance_tenancy              = "default"
 
   tags = {
-    Name = var.ENV
+    Name                            = var.ENV
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                            = aws_vpc.main.id
+  cidr_block                      = "10.0.1.0/24"
+
+  tags = {
+    Name                          = "Main"
   }
 }
 
