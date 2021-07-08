dd0698759b90cdeb5ddb5a03c5aba82cd516823c
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-08 21:53:31 +0530

    made changes in route-tables.tf

--- vpc/route-tables.tf
+++ vpc/route-tables.tf
@@ -10,6 +10,6 @@ resource "aws_route_table" "public-rt" {
   vpc_id                                = aws_vpc.main.id
 
   tags = {
-    Name                               = "[public]-route-table"
+    Name                               = "public-route-table"
   }
 }
