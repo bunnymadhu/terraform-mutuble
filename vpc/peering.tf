2f459ef071fc826891a7cb0f2f6988d47d597b0d
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-08 09:17:18 +0530

    made changes in peering.tf

--- vpc/peering.tf
+++ vpc/peering.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc_peering_connection" "peer-connection" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.DEFAULT_VPC_ID
+  peer_vpc_id                  = aws_vpc.main.id
+  vpc_id                          = var.DEFAULT_VPC_ID
+  auto_accept                 = true
 }
 
 # peering connections is useful to connect to one vpc to another vpc....
+# auto_accept   = true is only accept the same account (owner account), if we goes beyond the another account it will nt accept
