42cb52b837060bed7f385e6b6d1002397dfc66c0
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-11 07:49:07 +0530

    made changes in mongodb.tf

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -12,29 +12,43 @@
 //## u can find the "aws spot instance terraform" -- https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/spot_instance_request to launch instances by using spot request..
 //
 //
-//resource "aws_security_group" "allow_mongodb" {
-//  name                                      = "allow_mongodb"
-//  description                              = "AllowMongoDB"
-//  vpc_id                                     = aws_vpc.main.id
-//
-//  ingress {
-//    description                           = "TLS from VPC"
-//    from_port                             = 443
-//    to_port                                 = 443
-//    protocol                               = "tcp"
-//    cidr_blocks                          = [aws_vpc.main.cidr_block]
-//    ipv6_cidr_blocks                  = [aws_vpc.main.ipv6_cidr_block]
-//  }
-//
-//  egress {
-//    from_port                           = 0
-//    to_port                               = 0
-//    protocol                             = "-1"
-//    cidr_blocks                        = ["0.0.0.0/0"]
-//    ipv6_cidr_blocks                = ["::/0"]
-//  }
-//
-//  tags = {
-//    Name                               = "allow_tls"
-//  }
-//}
+resource "aws_security_group" "allow_mongodb" {
+  name                                      = "allow_mongodb"
+  description                              = "AllowMongoDB"
+  vpc_id                                     = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description                           = "SSH"
+    from_port                             = 22
+    to_port                                 = 22
+    protocol                               = "tcp"
+    cidr_blocks                          = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  ## for SSH we need to allow these two networks(VPC_CIDR,DEFAULT_VPC_CIDR)....
+
+  ingress {
+    description                           = "MONGODB"
+    from_port                             = 27017
+    to_port                                 = 27017
+    protocol                               = "tcp"
+    cidr_blocks                          = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  ## for MONGODB we need to allow ONLY internal to this VPC not needed externalother vpc's...(VPC_CIDR)....
+  ## tcp----Transmission Control Protocol----TCP stands for Transmission Control Protocol a communications standard that enables application programs and computing devices to                                                                       exchange messages over a network. It is designed to send packets across the internet and ensure the successful delivery of data and                                                                        messages over networks..
+
+  egress {
+    from_port                           = 0
+    to_port                               = 0
+    protocol                             = "-1"
+    cidr_blocks                        = ["0.0.0.0/0"]
+    ipv6_cidr_blocks                = ["::/0"]
+  }
+
+  tags = {
+    Name                               = "AllowMongoDB"
+  }
+}
+
+## if  we give this data,,it allows in AWS security groups and  it will shows  inbound rules as allow_mongodb...
