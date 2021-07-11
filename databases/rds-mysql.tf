72e3f455d6a6e706fce0af28fde6b352ce1703b6
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-11 18:40:40 +0530

    made changes in rds-mysql.tf

--- databases/rds-mysql.tf
+++ databases/rds-mysql.tf
@@ -1,21 +1,21 @@
-resource "aws_db_instance" "default" {
-  identifier                            = "mysql-${var.ENV}"
-  allocated_storage               = 10
-  engine                               = "mysql"
-  engine_version                   = "5.7"
-  instance_class                    = "db.t3.micro"
-  name                                  = "default-launched"
-  username                           = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_MYSQL_USER"]
-  password                            = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_MYSQL_PASS"]
-  parameter_group_name       = "default.mysql5.7"
-  skip_final_snapshot             = true
-  vpc_security_group_ids       = [aws_security_group.allow_rds_mysql.id]
-  db_subnet_group_name       = aws_db_subnet_group.subnet-group.name
-  tags                                    = {
-    Name                                = "mysql-${var.ENV}"
-    Environment                      = var.ENV
-  }
-}
+//resource "aws_db_instance" "default" {
+//  identifier                            = "mysql-${var.ENV}"
+//  allocated_storage               = 10
+//  engine                               = "mysql"
+//  engine_version                   = "5.7"
+//  instance_class                    = "db.t3.micro"
+//  name                                  = "default-launched"
+//  username                           = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_MYSQL_USER"]
+//  password                            = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RDS_MYSQL_PASS"]
+//  parameter_group_name       = "default.mysql5.7"
+//  skip_final_snapshot             = true
+//  vpc_security_group_ids       = [aws_security_group.allow_rds_mysql.id]
+//  db_subnet_group_name       = aws_db_subnet_group.subnet-group.name
+//  tags                                    = {
+//    Name                                = "mysql-${var.ENV}"
+//    Environment                      = var.ENV
+//  }
+//}
 
 ## it is in terraform_rds_aws_db_instance
 ## we may go with instance in this  practice but in companies might go with clusters...same clusters are in  given path...
@@ -24,42 +24,42 @@ resource "aws_db_instance" "default" {
 
 ## it is in terraform_rds_aws_db_subnet group
 
-resource "aws_db_subnet_group" "subnet-group" {
-  name                                 = "mysql-db-group-${var.ENV}"
-  subnet_ids                         = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
-
-  tags = {
-    Name                               = "mysql-db-group-${var.ENV}"
-    Environment                     = var.ENV
-  }
-}
-
-resource "aws_security_group" "allow_rds_mysql" {
-  name                                = "allow_rds_mysql"
-  description                        = "AllowRdsMySQL"
-  vpc_id                              = data.terraform_remote_state.vpc.outputs.VPC_ID
-
-  ingress {
-    description                     = "MYSQL"
-    from_port                       = 3306
-    to_port                           = 3306
-    protocol                         = "tcp"
-    cidr_blocks                    = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
-  }
+//resource "aws_db_subnet_group" "subnet-group" {
+//  name                                 = "mysql-db-group-${var.ENV}"
+//  subnet_ids                         = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+//
+//  tags = {
+//    Name                               = "mysql-db-group-${var.ENV}"
+//    Environment                     = var.ENV
+//  }
+//}
+//
+//resource "aws_security_group" "allow_rds_mysql" {
+//  name                                = "allow_rds_mysql"
+//  description                        = "AllowRdsMySQL"
+//  vpc_id                              = data.terraform_remote_state.vpc.outputs.VPC_ID
+//
+//  ingress {
+//    description                     = "MYSQL"
+//    from_port                       = 3306
+//    to_port                           = 3306
+//    protocol                         = "tcp"
+//    cidr_blocks                    = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+//  }
 
   ## for MONGODB we need to allow ONLY internal to this VPC not needed externalother vpc's...(VPC_CIDR)....
   ## tcp----Transmission Control Protocol----TCP stands for Transmission Control Protocol a communications standard that enables application programs and computing devices to                                                                       exchange messages over a network. It is designed to send packets across the internet and ensure the successful delivery of data and                                                                        messages over networks..
 
-  egress {
-    from_port                       = 0
-    to_port                           = 0
-    protocol                         = "-1"
-    cidr_blocks                    = ["0.0.0.0/0"]
-    ipv6_cidr_blocks            = ["::/0"]
-  }
-
-  tags                               = {
-    Name                           = "AllowRdsMySQL"
-    Environment                  = var.ENV
-  }
-}
+//  egress {
+//    from_port                       = 0
+//    to_port                           = 0
+//    protocol                         = "-1"
+//    cidr_blocks                    = ["0.0.0.0/0"]
+//    ipv6_cidr_blocks            = ["::/0"]
+//  }
+//
+//  tags                               = {
+//    Name                           = "AllowRdsMySQL"
+//    Environment                  = var.ENV
+//  }
+//}
