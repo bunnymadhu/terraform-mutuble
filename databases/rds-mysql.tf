5e1f0bce941affab41eed0af3ec20f393d394997
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-12 18:31:06 +0530

    made changes in mongodb.tf

--- databases/rds-mysql.tf
+++ databases/rds-mysql.tf
@@ -14,6 +14,7 @@ resource "aws_db_instance" "default" {
   tags                                    = {
     Name                                = "mysql-${var.ENV}"
     Environment                      = var.ENV
+
   }
 }
 
