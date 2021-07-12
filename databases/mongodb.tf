a7e5ab44739c985ba8b6644b0e281bb5a79c7aa6
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-12 07:57:34 +0530

    made changes in rds-mysql.tf

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "mongodb" {
   ami                                         = data.aws_ami.centos7.id
-  spot_price                               = "0.0036"
-  instance_type                          = "t2.micro"
+  spot_price                               = "0.0032"
+  instance_type                          = "t3.micro"
   vpc_security_group_ids            = [aws_security_group.allow_mongodb.id]
   subnet_id                                = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS[1]
   wait_for_fulfillment                   = true
