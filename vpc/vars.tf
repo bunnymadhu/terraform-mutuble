e5e184b59bd28b9019ae38085b9b0bb774cd4576
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-16 08:07:10 +0530

    made changes in rabbitmq.tf

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -6,3 +6,15 @@ variable "SUBNET_ZONES"                          {}
 variable "DEFAULT_VPC_ID"                       {}
 variable "DEFAULT_VPC_ROUTE_TABLE"   {}
 variable "DEFAULT_VPC_CIDR"                  {}
+
+
+
+
+
+
+
+
+
+
+
+## What does CIDR mean--CIDR stands for Classlesss Inter-Domain Routing. It enables network administrators to group blocks of IP addresses into single routing networks. CIDR                                               accomplishes the same task as traditional subnet masking. To understand what CIDR notation means, take the IP address 192.168.
