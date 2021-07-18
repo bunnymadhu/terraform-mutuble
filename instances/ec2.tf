6e25a2a0b2259b92d0159945a75a12666b0327e2
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-18 06:06:13 +0530

    make changes in ec2.tf

--- instances/ec2.tf
+++ instances/ec2.tf
@@ -82,3 +82,19 @@ resource "null_resource" "ansible-apply" {
 
   }
 }
+
+## it is in aws_targetgroup_terraform
+
+resource "aws_lb_target_group" "target-group" {
+  name                           = "${var.COMPONENT}-${var.ENV}"
+  port                             = var.PORT
+  protocol                       = "HTTP"
+  vpc_id                         = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+resource "aws_lb_target_group_attachment" "tg-attach" {
+  count                          = var.INSTANCE_COUNT
+  target_group_arn         = aws_lb_target_group.target-group.arn
+  target_id                     = element(aws_spot_instance_request.instances.*.spot_instance_id, count.index )
+  port                            = var.PORT
+}
