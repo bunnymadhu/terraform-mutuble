746920f163b9a93acdf9bd3b549a35cfa5beb75c
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-19 08:48:20 +0530

    added content in vpc/vpc.tf

--- vpc/route53.tf
+++ vpc/route53.tf
@@ -4,5 +4,5 @@ resource "aws_route53_zone_association" "secondary" {
 }
 
 
-## in route53,there is edit hosted zone,in that vpc creditinals are there,in that we have update thru automation thats y we have to update it
+## in route53,there is edit hosted zone,in that vpc credentials are there,in that we have update thru automation thats y we have to update it
 ## go to in chrome VPCs to associate with the hosted zone terraform in that copy from the above Resource..
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,6 +1,11 @@
 resource "aws_vpc" "main" {
   cidr_block                         = var.VPC_CIDR
   instance_tenancy              = "default"
+  enable_dns_hostnames     = true
+  enable_dns_support          = true
+
+  ## cat /etc/nginx/default.d/roboshop.conf
+  ## nslookup catalogue-dev.roboshop.internal,,,,,,, we added route53.tf for vpc dns so wait for the dns request and we will go to VPC in aws in that whatever u run the vpc it can create one dev right,select and  in that options both enabled DNS Hostnames,DNS Resolution Support...
 
   tags = {
     Name                            = var.ENV
