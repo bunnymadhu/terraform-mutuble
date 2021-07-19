3826162e1efd662d0a3e38a9b12144d24cd687a5
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-07-19 08:22:16 +0530

    added templates from frontend in ansible/templates

--- alb/public-lb.tf
+++ alb/public-lb.tf
@@ -42,3 +42,28 @@ resource "aws_security_group" "allow_public_lb" {
     Name                               = "AllowPublicLB"
   }
 }
+
+resource "aws_lb_target_group" "frontend-target-group" {
+  name                           = "frontend-${var.ENV}"
+  port                             = 80
+  protocol                       = "HTTP"
+  vpc_id                         = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    path                          = "/"
+    port                           = 80
+    interval                      = 10
+  }
+}
+
+## aws_lb_listener terraform in chrome,and why it is public means for frontend all the data comes from the public agent only but whereas other components gets from the information is private agent.
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn             = aws_lb.public.arn
+  port                                  = "80"
+  protocol                            = "HTTP"
+
+  default_action {
+    type                               = "forward"
+    target_group_arn            = aws_lb_target_group.frontend-target-group.arn
+  }
+}
---
+++ vpc/route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_zone_association" "secondary" {
+  zone_id = var.HOSTED_ZONE_ID
+  vpc_id  = aws_vpc.main.id
+}
+
+## in route53,there is edit hosted zone,in that vpc creditinals are there,in that we have update thru automation thats y we have to update it
+## go to in chrome VPCs to associate with the hosted zone terraform in that copy from the above Resource..
